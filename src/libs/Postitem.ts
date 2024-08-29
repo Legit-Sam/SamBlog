e29a75af8e491bc866dc916c8638666a0985ee0f
Legit-Sam <webdeveloper2167@gmail.com>
2024-08-29 08:51:17 +0100

    Initial Comment

--- src/libs/Postitem.ts
+++ src/libs/Postitem.ts
@@ -1,22 +1,23 @@
 import type {
-    PortableTextBlock,
+  PortableTextBlock,
+  PortableTextMarkDefinition,
+  PortableTextSpan,
+} from "@portabletext/types";
+
+export interface PostItem {
+  _id?:string;
+  name: string;
+  slug: string;
+  content: PortableTextBlock<
     PortableTextMarkDefinition,
     PortableTextSpan,
-  } from "@portabletext/types";
-  
-  export interface PostItem {
-    name: string;
-    slug: string;
-    content: PortableTextBlock<
-      PortableTextMarkDefinition,
-      PortableTextSpan,
-      string,
-      string
-    >[];
-    image: ImageMetadata;
-    date: string;
-    author: string;
-    category: string;
-    featured: boolean;
-    tags: string[];
-  }
+    string,
+    string
+  >[];
+  image: ImageMetadata;
+  date: string;
+  author: string;
+  category: string;
+  featured: boolean;
+  tags: string[];
+}
